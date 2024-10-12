cb9c83e24985c13805e396e5f5c63120a8a940cf
RudraBajaj <rudrabajajdabwali@gmail.com>
2024-10-13 03:49:24 +0530

    animation of text , theme changed to dark

---
+++ index.js
@@ -0,0 +1,13 @@
+let letters = document.querySelectorAll('.text');
+let index = 0;
+
+setInterval(() => {
+    letters.forEach((letter, i) => {
+        if (i === index) {
+            letter.style.color = `hsl(${Math.random() * 360}, 100%, 70%)`; // Brighter colors
+        } else {
+            letter.style.color = '#cccccc'; // Reset other letters to light gray
+        }
+    });
+    index = (index + 1) % letters.length; // Loop through letters
+}, 200);
